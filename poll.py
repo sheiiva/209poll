7ef872b523c35948fa3e52f652bf712cb0ef33da
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-05-13 10:38:51 +0200

    [ADD] population size, sample size and voting intention printing

--- poll.py
+++ poll.py
@@ -9,6 +9,8 @@
 ############################################
 
 
+from sys import argv
+
 from compute import Compute
 
 
@@ -19,7 +21,33 @@ class Poll():
     """
 
     def __init__(self):
-        self._p = 0
+        self._pSize = int(argv[1])
+        self._sSize = int(argv[2])
+        self._p = float(argv[3])
+
+    def showPopulationSize(self) -> None:
+        
+        """
+        Print out the population size.
+        """
+
+        print("Population size:\t{}".format(self._pSize))
+
+    def showSampleSize(self) -> None:
+
+        """
+        Print out the sample size.
+        """
+
+        print("Sample size:\t\t{}".format(self._sSize))
+
+    def showVotingIntentions(self) -> None:
+
+        """
+        Print out the votin intention percentage.
+        """
+
+        print("Voting intentions:\t{:.2f}%".format(self._p))
 
     def run(self) -> None:
 
@@ -27,4 +55,6 @@ class Poll():
         Run computations and process output printing.
         """
 
-        return
+        self.showPopulationSize()
+        self.showSampleSize()
+        self.showVotingIntentions()
