c82c45c52075c9da944e1c2a56cee2c56ec6c9d2
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-05-13 10:13:17 +0200

    [ADD] usage

---
+++ usage.py
@@ -0,0 +1,24 @@
+############################################
+#                MATHEMATICS               #
+############################################
+#                                          #
+#  MONFA-MATAS Patricica & ROZET Corentin  #
+#                                          #
+#           Project : 209poll_2019         #
+#                                          #
+############################################
+
+
+def help() -> None:
+
+    """
+    Show usage of the program.
+    """
+
+    print("USAGE\n"
+        "\t./209poll pSize sSize p\n"
+        "\n"
+        "DESCRIPTION\n"
+        "\tpSize   size of the populations\n"
+        "\tSize    size of the sample (supposed to be representative)\n"
+        "\tp       percentage of voting intentions for a specific candidate")
