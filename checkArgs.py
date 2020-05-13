f451f01c641c8bba01c1fb2b06370aebd3cb0f0a
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-05-13 10:30:05 +0200

    [ADD] checkArgs

--- checkArgs.py
+++ checkArgs.py
@@ -17,6 +17,38 @@ class ArgumentManager():
         Check for arguments validity.
         """
 
+        def isInt(value) -> bool:
+            try:
+                int(value)
+            except ValueError:
+                return False
+            else:
+                return True
+
+        def isFloat(value) -> bool:
+            try:
+                float(value)
+            except ValueError:
+                return False
+            else:
+                return True
+
+        if len(argv) != 4:
+            print("Wrong number of arguments. Please retry with -h.")
+            return 84
+        if not isInt(argv[1]) or not isInt(argv[2]) or not isFloat(argv[3]):
+            print("Wrong argument type. Please retry with -h.")
+            return 84
+        elif int(argv[2]) >= int(argv[1]):
+            print("Size of the sample might be smaller than the size of the population. Please retry with -h.")
+            return 84
+        elif int(argv[1]) <= 0 or int(argv[2]) <= 0:
+            print("Wrong value. Please retry with -h.")
+            return 84
+        elif float(argv[3]) < 0.0 or float(argv[3]) > 100.0:
+            print("Wrong value. Please retry with -h.")
+            return 84
+
         return 0
 
     def needHelp(self, argv) -> bool:
